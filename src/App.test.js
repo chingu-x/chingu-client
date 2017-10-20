f6977647abb22631fb8df7e423a9e43c599d6ddd
Oxyrus <im.oxyrus@gmail.com>
2017-10-20 11:41:45 -0500

    Setup enzyme and jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+// Configure Enzyme adapter
+Enzyme.configure({ adapter: new Adapter() });
+
+it('renders app component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
 });
