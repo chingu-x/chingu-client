6c2e161c241fa60e15e4b1bc0121bfa5f2e11a16
Oxyrus <im.oxyrus@gmail.com>
2017-11-18 14:00:27 -0500

    Configure apollo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { HttpLink } from "apollo-link-http";
 import { BrowserRouter } from "react-router-dom";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:5000/graphql" }),
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
   document.getElementById("root")
 );
 registerServiceWorker();
