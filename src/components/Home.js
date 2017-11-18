7c23b8e27cfad76f1f01080cbf0aa0465a682e3c
Oxyrus <im.oxyrus@gmail.com>
2017-11-18 17:25:28 -0500

    Move to respective folders

--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Home extends Component {
-  render() {
-    return <h1>Hello from the home component</h1>;
-  }
-}
-
-export default Home;
--- src/components/profile/Profile.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Profile extends Component {
-  render() {
-    return <h1>Hello from the profile component</h1>;
-  }
-}
-
-export default Profile;
