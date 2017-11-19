adcf587e2c187993ee0f68bb0beef3b2b5bfb2bd
Oxyrus <im.oxyrus@gmail.com>
2017-11-19 11:38:49 -0500

    Use state to handle login form

--- src/components/Login/Form/index.js
+++ src/components/Login/Form/index.js
@@ -1,27 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Form = props => (
-  <div className="row">
-    <form className="col s12">
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleLogin(e) {
+    e.preventDefault();
+    console.log("The form was submitted");
+  }
+
+  render() {
+    return (
       <div className="row">
-        <div className="input-field col s12">
-          <input id="email" type="email" className="validate" />
-          <label htmlFor="email">Email</label>
-        </div>
+        <form onSubmit={e => this.handleLogin(e)} className="col s12">
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                id="email"
+                type="email"
+                className="validate"
+                value={this.state.email}
+                onChange={e => this.setState({ email: e.target.value })}
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                id="password"
+                type="password"
+                className="validate"
+                value={this.state.password}
+                onChange={e => this.setState({ password: e.target.value })}
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+          </div>
+          <div className="row center">
+            <button
+              type="submit"
+              className="waves-effect waves-light light-blue accent-3 btn"
+            >
+              Login
+            </button>
+          </div>
+        </form>
       </div>
-      <div className="row">
-        <div className="input-field col s12">
-          <input id="password" type="password" className="validate" />
-          <label htmlFor="password">Password</label>
-        </div>
-      </div>
-      <div className="row center">
-        <a className="waves-effect waves-light light-blue accent-3 btn">
-          Login
-        </a>
-      </div>
-    </form>
-  </div>
-);
+    );
+  }
+}
 
 export default Form;
