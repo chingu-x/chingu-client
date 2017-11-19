b3708bd92dc2272e66cae9e8ae72b5abd262f1f4
Oxyrus <im.oxyrus@gmail.com>
2017-11-18 21:57:48 -0500

    Add Link to Login page

--- src/components/Misc/Navbar.js
+++ src/components/Misc/Navbar.js
@@ -17,6 +17,9 @@ const Navbar = props => (
           <Link to="/signup">Sign up</Link>
         </li>
         <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
           <Link to="/">Logout</Link>
         </li>
       </ul>
