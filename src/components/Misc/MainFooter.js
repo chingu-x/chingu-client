3febb312a8de37b5a4c65e223be07970be24833d
Oxyrus <im.oxyrus@gmail.com>
2017-11-20 11:37:06 -0500

    Rewrite Footer to use antd

---
+++ src/components/Misc/MainFooter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from "antd";
+
+const { Footer } = Layout;
+
+const MainFooter = props => (
+  <Layout>
+    <Footer style={{ textAlign: "center" }}>Made by Chingu X.</Footer>
+  </Layout>
+);
+
+export default MainFooter;
