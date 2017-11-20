c0a4a05cd4137b9ce249116765eae6f26bc433b1
Oxyrus <im.oxyrus@gmail.com>
2017-11-20 11:36:49 -0500

    Rewrite Navbar to use antd

--- src/components/Misc/Navbar.js
+++ src/components/Misc/Navbar.js
@@ -1,39 +1,39 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Layout, Menu } from "antd";
 
 import "./Navbar.css";
 
-class Navbar extends Component {
-  handleLogout(e) {
-    e.preventDefault();
+const { Header } = Layout;
 
+class Navbar extends Component {
+  handleLogout = e => {
+    e.preventDefault();
     window.localStorage.removeItem("token");
     window.location = "/";
-  }
+  };
 
   render() {
     return (
-      <nav className="green accent-3">
-        <div className="nav-wrapper">
-          <Link to="/" className="brand-logo">
-            친구
-          </Link>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <Link to="/profile/2">Profile</Link>
-            </li>
-            <li>
+      <Layout>
+        <Header className="nav-header">
+          <div className="logo">Chingu</div>
+          <Menu theme="light" mode="horizontal" style={{ lineHeight: "64px" }}>
+            <Menu.Item key="1">
+              <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item key="2">
               <Link to="/apply">Apply</Link>
-            </li>
-            <li>
+            </Menu.Item>
+            <Menu.Item key="3">
               <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <a onClick={e => this.handleLogout(e)}>Logout</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <a onClick={this.handleLogout}>Logout</a>
+            </Menu.Item>
+          </Menu>
+        </Header>
+      </Layout>
     );
   }
 }
