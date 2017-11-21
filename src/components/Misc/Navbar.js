d7145a2a0a5fdbb600fa0e844ad49d72c6cd005b
Oxyrus <im.oxyrus@gmail.com>
2017-11-20 19:24:41 -0500

    Show different links based on user status

--- src/components/Misc/Navbar.js
+++ src/components/Misc/Navbar.js
@@ -30,15 +30,30 @@ class Navbar extends Component {
             <Menu.Item key="1">
               <Link to="/">Home</Link>
             </Menu.Item>
-            <Menu.Item key="2">
-              <Link to="/apply">Apply</Link>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Link to="/login">Login</Link>
-            </Menu.Item>
-            <Menu.Item key="4">
-              <a onClick={this.handleLogout}>Logout</a>
-            </Menu.Item>
+
+            {this.state.user_id ? null : (
+              <Menu.Item key="2">
+                <Link to="/signup">Sign Up</Link>
+              </Menu.Item>
+            )}
+
+            {this.state.user_id ? null : (
+              <Menu.Item key="3">
+                <Link to="/login">Login</Link>
+              </Menu.Item>
+            )}
+
+            {this.state.user_id ? (
+              <Menu.Item key="4">
+                <Link to={`/profile/${this.state.user_id}`}>Profile</Link>
+              </Menu.Item>
+            ) : null}
+
+            {this.state.user_id ? (
+              <Menu.Item key="5">
+                <a onClick={this.handleLogout}>Logout</a>
+              </Menu.Item>
+            ) : null}
           </Menu>
         </Header>
       </Layout>
