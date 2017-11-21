08cc1434a36483706930158562e7c0dd944bfe25
Oxyrus <im.oxyrus@gmail.com>
2017-11-20 19:27:10 -0500

    Add key to elements

--- src/components/Profile/Project/index.js
+++ src/components/Profile/Project/index.js
@@ -15,32 +15,30 @@ const Project = props => (
     <div className="content">
       <h3>{props.title}</h3>
       <p>{props.description}</p>
-      <p>
-        <ul>
-          <li>
-            {props.github_url ? (
-              <a href={props.github_url}>Github</a>
-            ) : (
-              "No GitHub"
-            )}
-          </li>
-          <li>
-            {props.project_url ? (
-              <a href={props.project_url}>Live</a>
-            ) : (
-              "No Project URL"
-            )}
-          </li>
-        </ul>
-      </p>
-      <p>
-        <strong>Team</strong>
-        <ul>
-          {props.users.map(user => {
-            return <li>{user.username}</li>;
-          })}
-        </ul>
-      </p>
+
+      <ul>
+        <li>
+          {props.github_url ? (
+            <a href={props.github_url}>Github</a>
+          ) : (
+            "No GitHub"
+          )}
+        </li>
+        <li>
+          {props.project_url ? (
+            <a href={props.project_url}>Live</a>
+          ) : (
+            "No Project URL"
+          )}
+        </li>
+      </ul>
+
+      <strong>Team</strong>
+      <ul>
+        {props.users.map(user => {
+          return <li key={user.username}>{user.username}</li>;
+        })}
+      </ul>
     </div>
   </Card>
 );
