ea25a7d08004ffbe30cdb8dfd927f132dc03442e
Oxyrus <im.oxyrus@gmail.com>
2017-11-24 20:30:12 -0500

    Update import path of components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 
-import Navbar from "./Misc/Navbar";
+import Navbar from "./Shared/Navbar";
 import Home from "./Home/Home";
 import Profile from "./Profile/Profile";
+import Settings from "./Profile/Settings";
 import SignUp from "./SignUp/SignUp";
 import Login from "./Login/Login";
-import Footer from "./Misc/MainFooter";
+import Footer from "./Shared/MainFooter";
 import "./App.css";
 
 class App extends Component {
@@ -17,6 +18,7 @@ class App extends Component {
 
         <Route exact path="/" component={Home} />
         <Route path="/profile/:id" component={Profile} />
+        <Route path="/settings" component={Settings} />
         <Route path="/signup" component={SignUp} />
         <Route path="/login" component={Login} />
 
