e403c0e21afa7bad229fff1e8239e9c630f37089
Oxyrus <im.oxyrus@gmail.com>
2017-11-25 10:39:31 -0500

    Add register user mutation

---
+++ src/components/SignUp/registerUserMutation.js
@@ -0,0 +1,29 @@
+import gql from "graphql-tag";
+
+const registerUser = gql`
+  mutation registerUser(
+    $first_name: String!
+    $last_name: String!
+    $email: String!
+    $github_url: String!
+    $password: String!
+  ) {
+    createUser(
+      user_data: {
+        first_name: $first_name
+        last_name: $last_name
+        github_url: $github_url
+      }
+      email: $email
+      password: $password
+      email: $email
+    ) {
+      jwt
+      user {
+        id
+      }
+    }
+  }
+`;
+
+export default registerUser;
