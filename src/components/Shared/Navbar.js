d8688ecee80b1239d0884783af9a4b2007a0b14a
Oxyrus <im.oxyrus@gmail.com>
2017-11-24 22:03:19 -0500

    Move Navbar component

---
+++ src/components/Shared/Navbar.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import { Link } from "react-router-dom";
+import { Layout, Menu } from "antd";
+
+import currentUserQuery from "./currentUserQuery";
+import "./Navbar.css";
+
+const { Header } = Layout;
+
+export class Navbar extends Component {
+  handleLogout = e => {
+    e.preventDefault();
+    window.localStorage.removeItem("token");
+    window.location = "/";
+  };
+
+  render() {
+    let { loading, user } = this.props.data;
+    if (loading) {
+      return <h1>Loading</h1>;
+    }
+    return (
+      <Layout>
+        <Header className="nav-header">
+          <div className="logo">Chingu</div>
+          <Menu theme="light" mode="horizontal" style={{ lineHeight: "64px" }}>
+            <Menu.Item key="1">
+              <Link to="/">Home</Link>
+            </Menu.Item>
+
+            {user ? null : (
+              <Menu.Item key="2">
+                <Link to="/signup">Sign Up</Link>
+              </Menu.Item>
+            )}
+
+            {user ? null : (
+              <Menu.Item key="3">
+                <Link to="/login">Login</Link>
+              </Menu.Item>
+            )}
+
+            {user ? (
+              <Menu.Item key="4">
+                <Link to={`/profile/${user.id}`}>Profile</Link>
+              </Menu.Item>
+            ) : null}
+
+            {user ? (
+              <Menu.Item key="5">
+                <Link to="/settings">Settings</Link>
+              </Menu.Item>
+            ) : null}
+
+            {user ? (
+              <Menu.Item key="6">
+                <a onClick={this.handleLogout}>Logout</a>
+              </Menu.Item>
+            ) : null}
+          </Menu>
+        </Header>
+      </Layout>
+    );
+  }
+}
+
+export default graphql(currentUserQuery)(Navbar);
